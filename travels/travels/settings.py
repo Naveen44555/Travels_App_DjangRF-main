53e45827afab60491d3e30bb2f2bde06a04955ef
Naveen44555 <naveennaveenyadav386@gmail.com>
2026-02-18 16:40:07 +0530

    requirements file

--- travels/travels/settings.py
+++ travels/travels/settings.py
@@ -103,15 +103,18 @@ WSGI_APPLICATION = 'travels.wsgi.application'
 
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': os.getenv('DB_NAME'),
-    'USER': os.getenv('DB_USER'),
-    'PASSWORD': os.getenv('DB_PASSWORD'),
-    'HOST': os.getenv('DB_HOST'),
-    'PORT': os.getenv('DB_PORT'),
-    'OPTIONS': {'autocommit': True},
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'ssl': {'ssl_mode': 'REQUIRED'},
+            'autocommit': True,
+        },
+    }
 }
 
 # Password validation
